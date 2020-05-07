913af4b9e6733180d74f8ef466bf526f253fc23a
Andrew <aybwww@gmail.com>
2020-05-07 09:02:37 +0500

    Create pdo.php

---
+++ pdo.php
@@ -0,0 +1,35 @@
+<?php
+
+$driver = 'mysql'; // тип базы данных, с которой мы будем работать
+
+$host = 'localhost';// альтернатива '127.0.0.1' - адрес хоста, в нашем случае локального
+
+$db_name = 'blog'; // имя базы данных
+
+$db_user = 'root'; // имя пользователя для базы данных
+
+$db_password = ''; // пароль пользователя
+
+$charset = 'utf8'; // кодировка по умолчанию
+
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+$dsn = "$driver:host=$host;dbname=$db_name;charset=$charset";
+$pdo = new PDO($dsn, $db_user, $db_password, $options);
+
+$sql = "SELECT * FROM table_name where id = :id";
+$statement = $pdo->prepare($sql);
+
+
+
+//  получившееся выражение выполняем методом execute(), 
+//  передавая в него массив с переменными, которые используются в запросе:
+
+
+$statement->execute($data); // НЕПОНЯТКА ЗДЕСЬ У МЕНЯ :(
+
+
+
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// var_dump($result);
+
