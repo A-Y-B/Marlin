d48ecde4434a281b02dc371107839db3b52ac907
Andrew <aybwww@gmail.com>
2020-05-08 14:59:20 +0500

    Create form.php

---
+++ form.php
@@ -0,0 +1,35 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            
+            
+                <form method="post">
+                    <div class="form-group">
+                        <input type="email" name="email" placeholder="email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" placeholder="password">
+                    </div>
+
+                        <button type="submit" class="btn btn-primary">Отправить</button>
+                </form>
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
